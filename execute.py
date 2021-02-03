a8d701edd5af90e809590c863dc725278c93f9bb
Dima G <gerasym@gmail.com>
2021-02-03 21:40:11 +0000

    cleaned up execute.py

--- execute.py
+++ execute.py
@@ -20,6 +20,7 @@ except:
 
 import importlib
 import models
+import sys
 import functions 
 import torch
 import torch.nn as nn
@@ -60,54 +61,3 @@ param_train_classifier = models.Param(channels = 3, epochs = 2, size = 256, name
 param_train_cycle = param_train_cycle_list[model_param_id-1]
 cycle.train(param_train_cycle)   
 sys.exit("Training done")     
-'''funktionieren:'''
-cycle.load_classifier()
-cycle.load_cycle_nets(epoch = 100)
-
-'''damit eval_testset sinnvoll funktioniert, muss man vorher einmal load_classifier und load_cycle_nets ausführen'''
-# eval_testset calculates the classifiers prediction for several pairs of original and generted images
-# it returns the examined pictures in batch-form and two lists storing the classifiers prediction for the batches respectively
-# if plot = True all chosen pairs as plottet with their classification scores
-org, gen, org_percentage, gen_percentage = cycle.eval_testset(param_eval_testset, source_domain= "A",pic_number_low =0 , pic_number_high = 1, plot = True, explain = False) 
-print(org_percentage)
-print(gen_percentage)
-
-''' scheint zu funktionieren, fuer h2z ist bereits ein classifier trainiert, fuer andere datensaetze noch nicht, nicht sonderlich fuer deepnote geeignet'''
-#cycle.train_classifier(param_train_classifier) 
-
-'''noch nicht implementiert / aus altem code ubernommen:'''
-# contiue_train
-
-
-
-import models
-import functions
-## das hier nur test. spaeter wieder loeschen
-genA2B = models.Small_Generator(input_nc=1, output_nc=1, n_residual_blocks=3)
-genB2A = models.Small_Generator(input_nc=1, output_nc=1, n_residual_blocks=3)
-discA  = models.Small_Discriminator(input_nc=1)
-discB  = models.Small_Discriminator(input_nc=1)
-
-# collect data
-functions.get_data_MNIST('fog',1000,100)
-pathA_Train = 'data/mnist2fog_1000/trainA/'
-pathB_Train = 'data/mnist2fog_1000/trainB/'
-pathA_Test = 'data/mnist2fog_1000/testA/'
-pathB_Test = 'data/mnist2fog_1000/testB/'
-# train
-cycle = models.CycleGAN(genA2B, genB2A, discA, discB, "dummy", "cpu")
-params = models.Param(
-    channels=1,
-    epochs=20,
-    size=28,
-    pathA_Train=pathA_Train,
-    pathB_Train=pathB_Train,
-    pathA_Test=pathA_Test,
-    pathB_Test=pathB_Test  
-)
-
-cycle.train(params)
-
-
-
-
